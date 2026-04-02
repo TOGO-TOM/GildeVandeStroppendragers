c61febdcd0091ef9a45ae1678627b65338af670a
Tom Goderis <tom.goderis@katoennatie.com>
2026-04-02 14:56:44 +0200

    Cleanup: remove AdminController migrate endpoint and DeploySecret config

---
+++ fix_isfilerable.sql
@@ -0,0 +1,5 @@
+﻿IF NOT EXISTS (SELECT 1 FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260402101122_AddCustomFieldIsFilterable')
+BEGIN
+    ALTER TABLE [CustomFields] ADD [IsFilterable] bit NOT NULL DEFAULT CAST(0 AS bit);
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion]) VALUES (N'20260402101122_AddCustomFieldIsFilterable', N'8.0.0');
+END
